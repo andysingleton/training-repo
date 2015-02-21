943df541643740328c8bb504ddb56822dca38d8e
Andy Singleton <andy.singleton@brightprocess.com>
2015-02-21 17:04:39 +0000

    Add flask app

---
+++ turret-app/turret.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/')
+def base():
+  return 'Are you still there?\n'
+
+@app.route('/target')
+def target():
+  return str(request.remote_addr)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0',port=8080
